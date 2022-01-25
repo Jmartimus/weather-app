77e8a302b9c91ef3e8e6b9145c4a512a029035a4
Jonathan Martinez <Jmartinez070589@gmail.com>
2022-01-24 20:12:53 -0600

    manipulating JSON.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-import React, { useCallback } from "react";
-import CatFacts from "./CatFacts";
-import useGetWeather from "./useGetWeather";
+import React, { useEffect, useState } from 'react';
+import CatFacts from './CatFacts';
+import useGetWeather from './useGetWeather';
 
 function App() {
-  const response = useGetWeather();
-  console.log(response);
+  const results = useGetWeather();
+  const getFirstDayTempHigh = () => {
+    if (results !== undefined) {
+      console.log(results);
+    }
+    return;
+  };
   return (
     <div>
       <CatFacts />
-      <button onClick={useGetWeather}>How's the weather?!</button>
+      <button onClick={getFirstDayTempHigh}>How's the weather?!</button>
     </div>
   );
 }
--- src/useGetWeather.ts
+++ src/useGetWeather.ts
@@ -1,15 +1,20 @@
-import { KEY } from "./key";
-import axios from "axios";
+import { KEY } from './Key';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 export const useGetWeather = () => {
+  const [weatherResults, setWeatherResults] = useState();
   const fetchWeather = async () => {
     const res = await axios.get(
       `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Qutiman%2C%20TX?unitGroup=metric&include=current&key=${KEY}&contentType=json`
     );
-    console.log(res.data);
-    return res.data;
+    // console.log(res.data);
+    setWeatherResults(res.data);
   };
-  fetchWeather();
+  useEffect(() => {
+    fetchWeather();
+  }, []);
+  return weatherResults;
 };
 
 // const fetchWeather = () => {
