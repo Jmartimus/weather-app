bd9cdaf738b387ebce91e53948cc9666a6fa57a9
Scott Thompson <sthompson082290@gmail.com>
2022-01-28 15:54:46 -0600

    Added multiple conditions for the current weather

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,39 @@ function App() {
       <CatFacts />
       <button onClick={getFirstDayTempHigh}>How's the weather?!</button>
       <div className="results-container">
-        <p>{results !== undefined ? results.latitude : ""}</p>
+        <p>
+          {" "}
+          Conditions:
+          {results !== undefined ? results.currentConditions.conditions : ""}
+        </p>
+        <p>
+          Feels Like:
+          {results !== undefined ? results.currentConditions.feelslike : ""}
+        </p>
+        <p>
+          Humidity:
+          {results !== undefined ? results.currentConditions.humidity : ""}
+        </p>
+        <p>
+          Precipitation:
+          {results !== undefined ? results.currentConditions.precip : ""}
+        </p>
+        <p>
+          Temp:
+          {results !== undefined ? results.currentConditions.temp : ""}
+        </p>
+        <p>
+          Sunrise time:
+          {results !== undefined ? results.currentConditions.sunrise : ""}
+        </p>
+        <p>
+          Sunset time:
+          {results !== undefined ? results.currentConditions.sunset : ""}
+        </p>
+        <p>
+          Wind:
+          {results !== undefined ? results.currentConditions.windspeed : ""}
+        </p>
       </div>
     </div>
   );
