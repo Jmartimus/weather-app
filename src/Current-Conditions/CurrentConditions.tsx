d1440d52250ea723acf6d68c5db966663bc78e34
Scott Thompson <sthompson082290@gmail.com>
2022-02-08 14:01:34 -0600

    added utils file with fahrenheit formula, passed to current conditions to clean up code lines 29 - 36

--- src/Current-Conditions/CurrentConditions.tsx
+++ src/Current-Conditions/CurrentConditions.tsx
@@ -4,6 +4,8 @@ import { useNavigate } from 'react-router-dom';
 import { WeatherResults } from '../API/constants';
 import { Nullable } from '../global';
 import { ReactComponent as Platypus } from '../icons/platypuslogo.svg';
+import { toFahrenheit } from '../utils';
+
 // import cloudy from '../icons/overcast-logo.svg';
 
 interface CurrentConditionsProps {
@@ -12,13 +14,12 @@ interface CurrentConditionsProps {
 }
 
 const CurrentConditions: React.FC<CurrentConditionsProps> = ({ setWeatherData, weatherData }) => {
-  const navigate = useNavigate();
+  const navigate = useNavigate(); //  useNavigate is a react-router hook
   const goHome = () => {
-    navigate('/');
-    setWeatherData(null);
+    navigate('/'); //  any place we use navigate it uses the navigate hook^
+    setWeatherData(null); //  we setWeatherData to null so that when we go back home it's empty for another user input
   };
-  console.log(weatherData);
-  console.log(weatherData!.days[0].tempmax);
+
   return (
     <div className="main-container">
       <div className="logo-container">
@@ -26,13 +27,13 @@ const CurrentConditions: React.FC<CurrentConditionsProps> = ({ setWeatherData, w
       </div>
       <div className="text-container">
         <h1>Today's Forcast: {weatherData!.currentConditions.conditions}</h1>
-        <h2>Temp: {(weatherData!.currentConditions.temp * 9) / 5 + 32}°F</h2>
-        <h4>Feels Like: {(weatherData!.currentConditions.feelslike * 9) / 5 + 32}°F</h4>
+        <h2>Temp: {toFahrenheit(weatherData!.currentConditions.feelslike)}°F</h2>
+        <h4>Feels Like: {toFahrenheit(weatherData!.currentConditions.feelslike)}°F</h4>
         <h4>Wind: {weatherData!.currentConditions.windspeed} mph</h4>
 
         <h4>
-          Hi: {Math.floor((weatherData!.days[0].tempmax * 9) / 5 + 32)}°F | Lo:{' '}
-          {Math.floor((weatherData!.days[0].tempmin * 9) / 5 + 32)}°F
+          Hi: {toFahrenheit(weatherData!.days[0].tempmax)}°F | Lo:{' '}
+          {toFahrenheit(weatherData!.days[0].tempmin)}°F
         </h4>
       </div>
       <Button variant="outlined" onClick={goHome}>
---
+++ src/utils.ts
@@ -0,0 +1,1 @@
+export const toFahrenheit = (data: number): string => Math.floor((data * 9) / 5 + 32).toString();
